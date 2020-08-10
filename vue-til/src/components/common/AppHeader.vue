81bcbdd151aa765ff204aa83c36841efb1e73cd2
cmj <chmin82@gmail.com>
2020-08-10 22:26:08 +0900

    copy from 6_vuex

--- vue-til/src/components/common/AppHeader.vue
+++ vue-til/src/components/common/AppHeader.vue
@@ -3,13 +3,17 @@
     <div>
       <router-link to="/" class="logo">
         TIL
+        <span v-if="isUserLogin">by {{ $store.state.username }}</span>
       </router-link>
     </div>
     <div class="navigations">
+      <!-- 1 -->
       <template v-if="isUserLogin">
-        <span class="username">{{ userName }}</span>
-        <a href="javascript:;" @click="logoutUser">Logout</a>
+        <a href="javascript:;" @click="logoutUser" class="logout-button">
+          Logout
+        </a>
       </template>
+      <!-- 2 -->
       <template v-else>
         <router-link to="/login">로그인</router-link>
         <router-link to="/signup">회원가입</router-link>
@@ -21,9 +25,6 @@
 <script>
 export default {
   computed: {
-    userName() {
-      return this.$store.state.username;
-    },
     isUserLogin() {
       return this.$store.getters.isLogin;
     },
@@ -70,6 +71,9 @@ a.logo {
   position: fixed;
   top: 0;
   width: 100%;
+}
+.logout-button {
+  font-size: 14px;
 }
 a.router-link-exact-active {
   color: white;
