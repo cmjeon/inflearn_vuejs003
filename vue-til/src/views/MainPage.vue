1fb59eea37829ccc183cfc33d937dc4cfbb75fd7
cmj <chmin82@gmail.com>
2020-08-11 23:29:32 +0900

    10-6

--- vue-til/src/views/MainPage.vue
+++ vue-til/src/views/MainPage.vue
@@ -3,18 +3,24 @@
     <div class="main list-container contents">
       <h1 class="page-header">Today I Learned</h1>
       <ul>
-        <li v-for="postItem in postItems" :key="postItem._id">
-          {{ postItem.title }}
-        </li>
+        <PostListItem
+          v-for="postItem in postItems"
+          :key="postItem._id"
+          :postItem="postItem"
+        ></PostListItem>
       </ul>
     </div>
   </div>
 </template>
 
 <script>
+import PostListItem from '@/components/posts/PostListItem.vue';
 import { fetchPosts } from '@/api/index';
 
 export default {
+  components: {
+    PostListItem,
+  },
   data() {
     return {
       postItems: [],
