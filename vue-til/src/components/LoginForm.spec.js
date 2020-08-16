0637d2ce80af1c776a7437f84602809419c0e729
cmj <chmin82@gmail.com>
2020-08-16 21:58:23 +0900

    19-9

--- vue-til/src/components/LoginForm.spec.js
+++ vue-til/src/components/LoginForm.spec.js
@@ -1,12 +1,10 @@
 import { shallowMount } from '@vue/test-utils';
-import Vue from 'vue';
 import LoginForm from './LoginForm.vue';
 
 describe('LoginForm.vue', () => {
-  test('컴포넌트가 마운트되면 화면에 그려져야 한다.', () => {
-    // const instance = new Vue(LoginForm).$mount();
+  test('ID는 이메일 형식이어야 한다.', () => {
     const wrapper = shallowMount(LoginForm);
-    // console.log(instance);
-    expect(wrapper.vm.username).toBe('');
+    const idInput = wrapper.find('#username');
+    console.log(idInput.html());
   });
 });
